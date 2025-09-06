c576ec3aa7bdd142b70e4168353a7328344f4048
Dan-Mechanics <github.tim@desmidt.org>
2025-09-06 19:11:09 +0200

    sfml

---
+++ StartingPoint/main.cpp
@@ -0,0 +1,20 @@
+#include "SFML/Graphics.hpp"
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode({ 800, 800 }), "Window Title");
+    sf::CircleShape shape(200.f, 100);
+    shape.setFillColor(sf::Color(204, 77, 5)); // Color circle
+    shape.setPosition({ 200, 200 }); // Center circle
+
+    while (window.isOpen())
+    {
+        
+
+        window.clear(sf::Color(18, 33, 43)); // Color background
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
